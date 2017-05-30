f67adeba815559113ed865874976e2b384f2873f
Martin Larsson <martin.larsson.post@gmail.com>
2017-05-30 11:35:11 +0200

    renders button

--- upg16/src/App.js
+++ upg16/src/App.js
@@ -14,12 +14,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Form from './Form.js';
+import Button from './Button.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
 		<Form />
+		<Button />
       </div>
     );
   }
